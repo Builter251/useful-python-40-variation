778ff6477858e8c6b8a8fda57c9280229a61982e
BUILTER <builter251@gmail.com>
2023-09-04 11:30:53 +0900

    Revert "웹페이지 캡처 커밋 (개발중)"
    This reverts commit 15c84e1cc57d308628f88ca095d34808b65cd3c8.

--- Webpage_Capture/Selenium_Test/selenium_test.py
+++
@@ -1,9 +0,0 @@
-# Selenium Test
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-print("start driver")
-driver.get("http://selenium.dev")
-print("get driver")
-driver.quit()
-print("quit driver")
